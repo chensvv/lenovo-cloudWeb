f52216f4e70cfa80dce2af6f3d7ee8cef306ff6f
chenjiasheng <chensvv@163.com>
2019-10-24 16:47:11 +0800

    显示tts

--- LenovoVoiceStage/common/mixjs/pagetop.js
+++ LenovoVoiceStage/common/mixjs/pagetop.js
@@ -143,8 +143,8 @@ function loadTop(data) {
     params +="							<a href=\'#\' target=\'_self\' data-toggle=\'dropdown\'>产品服务<span class=\'caret\'></span></a>";
 	params +="							<ul class=\'dropdown-menu\'>";
 	params +="								<li class=\'mobile_border\'><a href=\'#\' onclick=\'menu_voice_recognise()\'  id=\'second\'>短语音识别</a></li>";
-    params +="								<li><a href=\'#\' onclick=\'menu_voice_longrecognise()\'  id=\'\'>长语音识别 <span style=\'color:red\'>(Beta)</span></a></li>";
-    // params +="								<li><a href=\'#\' onclick=\'menu_voice_tts()\'  id=\'\'>语音合成 </a></li>";
+    params +="								<li class=\'mobile_border\'><a href=\'#\' onclick=\'menu_voice_longrecognise()\'  id=\'\'>长语音识别 <span style=\'color:red\'>(Beta)</span></a></li>";
+    params +="								<li class=\'mobile_border\'><a href=\'#\' onclick=\'menu_voice_tts()\'  id=\'\'>语音合成 </a></li>";
     params += "                        </ul>";
     params += "                    </li>";
 
