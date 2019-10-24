f09308e86c99688e51dc997e1cb985dca9da69a4
chenjiasheng <chensvv@163.com>
2019-10-24 16:15:52 +0800

    优化tts：对tts进行bug修改，重做

--- LenovoVoiceStage/product/mixjs/tts.js
+++ LenovoVoiceStage/product/mixjs/tts.js
@@ -15,7 +15,6 @@ function LenovoIdSyncLoginState(lenovoid_wust) {
                     'status': 'error'
                 };
             if (data.status == 'success') {
-                console.log(data)
                 window.localStorage.setItem('secretkey', data.secretkey);
                 window.localStorage.setItem('accountid', data.AccountID);
                 window.localStorage.setItem('lenovoname', data.name);
