34ded4705913f2c76e7598267e2c607fcc6e235a
chenjs9@lenovo.com_mac <chenjs9@lenovo.com>
2019-12-10 18:28:59 +0800

    修改分页显示

--- LenovoVoiceStage/forum/mixjs/list.js
+++ LenovoVoiceStage/forum/mixjs/list.js
@@ -102,7 +102,7 @@
 		pageSizeList:[5,10,15,20],//自定义分页数，默认[5,10,15,20,50]
 		defaultPageSize:10,//默认选中的分页数,默认选中第一个。如果未匹配到数组或者默认数组中，则也为第一个
 		isJump:false,//是否包含跳转功能，默认false
-		isPageNum:false,//是否显示分页下拉选择，默认false
+		isPageNum:true,//是否显示分页下拉选择，默认false
 		isPN:true,//是否显示上一页和下一面，默认true
 		isFL:false,//是否显示首页和末页，默认true
 		jump:function(curPage,pageSize){//跳转功能回调，传递回来2个参数，当前页和每页大小。如果没有设置分页下拉，则第二个参数永远为0。这里的this被指定为一个空对象，如果回调中需用到this请自行使用bind方法
