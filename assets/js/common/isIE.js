5f1c6d3d996ff82897b7655de4701644b3ef3fb4
chenjs <chenjs@lenovo.com>
2021-09-30 12:20:40 +0800

    修改小写判断条件

--- assets/js/common/isIE.js
+++ assets/js/common/isIE.js
@@ -1,9 +1,9 @@
 IEVersion()
 function IEVersion() {
     var userAgent = navigator.userAgent.toLowerCase();
-    // alert(userAgent)
-    var isIE = userAgent.indexOf("Trident") > -1 
-    var isEdge =  userAgent.indexOf("Edge") > -1
+    alert(userAgent)
+    var isIE = userAgent.indexOf("trident") > -1 
+    var isEdge =  userAgent.indexOf("edge") > -1
     // var chromeVer = userAgent.substr(userAgent.lastIndexOf("e/")+2).substring(0,2)
     var isQQ = RegExp(/qqbrowser/).exec(userAgent)
     var isSouGou = RegExp(/sogoumobilebrowser/).exec(userAgent)
