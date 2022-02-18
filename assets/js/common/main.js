7323878e6292551eb227caa34ffaabb6d74ef9fb
chenjs9 <chenjs9@lenovo.com>
2022-02-18 15:24:12 +0800

    hotfix

--- assets/js/common/main.js
+++ assets/js/common/main.js
@@ -156,9 +156,9 @@ var i18n = new EhiI18n('../lan/', function(){
     $('.nav-menu').removeClass('dis-flex')
   }
   if(localStorage.getItem('ehiI18n.Language') == 'zh' || localStorage.getItem('ehiI18n.Language') == '' || localStorage.getItem('ehiI18n.Language') == 'null' || localStorage.getItem('ehiI18n.Language') == undefined){
-    $('.trans').attr('src',' ../assets/img/trans2.png')
+    $('.trans').attr('src','https://voice.lenovomm.com/voicePlatform/assets/img/trans2.png')
   }else{
-    $('.trans').attr('src',' ../assets/img/trans.png')
+    $('.trans').attr('src','https://voice.lenovomm.com/voicePlatform/assets/img/trans.png')
   }
 
   // var localStorageData = JSON.parse(localStorage.getItem('data'))
@@ -224,23 +224,23 @@ function logout(){
   localStorage.clear();
   var url = window.location.href
   window.localStorage.setItem('returnurl',url)
-  window.location.href = ' ../login/login.html'
+  window.location.href = 'https://voice.lenovomm.com/voicePlatform/login/login.html'
 }
 
 function gologin(){
   var url = window.location.href
   window.localStorage.setItem('returnurl',url)
-  window.location.href = ' ../login/login.html'
+  window.location.href = 'https://voice.lenovomm.com/voicePlatform/login/login.html'
 }
 
 function isLang(){
   if(localStorage.getItem('ehiI18n.Language') == 'zh' || localStorage.getItem('ehiI18n.Language') == '' || localStorage.getItem('ehiI18n.Language') == 'null' || localStorage.getItem('ehiI18n.Language') == undefined){
     i18n.setLanguage('us')
-    $('.trans').attr('src',' ../assets/img/trans.png')
+    $('.trans').attr('src','https://voice.lenovomm.com/voicePlatform/assets/img/trans.png')
     // console.log("en===============")
   }else if(localStorage.getItem('ehiI18n.Language') == 'us'){
     i18n.setLanguage('zh')
-    $('.trans').attr('src',' ../assets/img/trans2.png')
+    $('.trans').attr('src','https://voice.lenovomm.com/voicePlatform/assets/img/trans2.png')
     // console.log("中文===============")
   }
 }
