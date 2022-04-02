a95f1e6a117f2d3f470c333ec5d58797a5bea267
chenjs9 <chenjs9@lenovo.com>
2022-04-02 10:41:19 +0800

    修复已知问题

--- assets/js/common/url.js
+++ assets/js/common/url.js
@@ -1,4 +1,4 @@
-// var sdkurl = 'http://10.110.148.59:8084'
+// var sdkurl = 'http://10.110.148.59:8085'
 // var sdkurl = 'http://10.110.148.59:8080'
 // var proURL = `${sdkurl}/lasf`
 var sdkurl = 'https://voice.lenovomm.com'
