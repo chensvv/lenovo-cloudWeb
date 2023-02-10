38c4412fb98c3cb27e97fe9a8105a1dd8cdab2c3
chenjs <chenjs9@lenovo.com>
2023-02-10 16:53:03 +0800

    新增授权相关sdk以及内容

--- assets/js/forum/questionlist.js
+++ assets/js/forum/questionlist.js
@@ -299,5 +299,62 @@ function questionDetail(val){
     window.location.href = './questiondetail.html?article='+val
 }
 $('.push-btn').click(function(){
-    window.location.href = './questionnew.html'
+        var params = {
+            username:$.base64.decode(window.localStorage.getItem('un')),
+            lenovoid:$.base64.decode(window.localStorage.getItem('acd')),
+            t:$.base64.decode(window.localStorage.getItem('token')),
+            u:"",
+            p:"",
+            language:"",
+            phone:"",
+            company:"",
+            dept:"",
+            lid:"",
+            opwd:"",
+            pwd:"",
+            userService:"",
+            code:"",
+            imgCode:"",
+            ucode:"",
+        }
+        var stringParams = JSON.stringify(params,userReplacer).replace(/\"/g, "").replace(/\:/g, '=').replace(/\,/g, '&').replace(/\{/g, '').replace(/\}/g, '')
+        params.sign = md5(stringParams)
+        $.ajax({
+            type:"POST",
+            url:proURL+"/userinfo",
+            headers: {  
+            "channel" : "cloudasr"
+            },
+            data:params,
+            success:function(data){
+                if(data.errorcode ==1024){
+                    localStorage.clear()
+                    Swal.fire({
+                        text: $.i18n.prop('logTimeOut'),
+                        showCancelButton: true,
+                        allowOutsideClick:false,
+                        allowEscapeKey:false,
+                        reverseButtons:true,
+                        width:'16em',
+                        confirmButtonColor: '#94cb82',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: $.i18n.prop('confirm'),
+                        cancelButtonText:$.i18n.prop('cancel')
+                    }).then((result)=>{
+                        if (result.isConfirmed) {
+                            var url = window.location.href
+                            window.localStorage.setItem('returnurl',url)
+                            window.location.href = '../login/login.html'
+                        }
+                    })
+                }else{
+                    window.location.href = './questionnew.html'
+                }
+            },
+            error:function(){
+                $('.user').css("display","none")
+                $('.login-reg').css("display","block")
+            }
+        })
+    
 })
